5cfb7dfea6bb1fef2b126cb90f21c047b10aec65
Bill Allaire <this@that.com>
2021-03-13 18:32:14 -0500

    Hash set method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AlgoTwoNumberSum
 {
@@ -6,7 +7,39 @@ namespace AlgoTwoNumberSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int[] numbers = { 3, 5, -4, 8, 11, 1, -1, 6 };
+            //int[] numbers = { 4, 6 };
+            int[] numbers = { 15 };
+            const int target = 10;
+
+            int[] result = TwoNumberSum(numbers, target);
+
+            if (result != null)
+            {
+                Console.WriteLine($"Result: {result[0]},{result[1]}");
+            }
+            else
+            {
+                Console.WriteLine("No match found");
+            }
+        }
+
+        public static int[] TwoNumberSum(int[] array, int targetSum)
+        {
+            HashSet<int> compliment = new HashSet<int>();
+
+            foreach(int x in array)
+            {
+                int y = targetSum - x;
+                if (compliment.Contains(y))
+                {
+                    return new int[] { x, y };
+                }
+
+                compliment.Add(x);
+            }
+
+            return new int[0];
         }
     }
 }
